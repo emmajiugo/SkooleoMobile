60586607ffa049d48c016d08b8edf2d81864c1b6
Patrick Oramah <patrickoramah98@gmail.com>
2020-12-26 19:49:58 +0100

    base url

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -3,6 +3,7 @@ import 'package:hexcolor/hexcolor.dart';
 const String ACCESS_TOKEN = 'ACCESS_TOKEN';
 
 const String BASE_URL = 'https://skooleo.com/mobile/api/v1';
+// const String BASE_URL = 'https://skooleo-mobile-api.herokuapp.com/api/v1';
 
 HexColor kPrimaryColor = HexColor('#5f55aa');
 HexColor kSecondaryColor = HexColor('#402c83');
